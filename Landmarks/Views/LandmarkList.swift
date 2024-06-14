87bea7693771f0a5baf59f169241aef604cf82a8
Lorenzo Mazzarotto <usomenac@icloud.com>
2024-06-14 20:02:14 -0300

    Create the list of landmarks

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Lorenzo Mazzarotto on 14/06/24.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
