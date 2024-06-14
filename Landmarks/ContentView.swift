06cafe488df9d74c6feaf268b31d7908e1e568f5
Lorenzo Mazzarotto <usomenac@icloud.com>
2024-06-14 19:18:19 -0300

    Create a new project and explore the canvas

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Lorenzo Mazzarotto on 14/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello SwiftUI!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Lorenzo Mazzarotto on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
