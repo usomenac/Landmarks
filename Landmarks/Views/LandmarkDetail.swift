893c15f2d2422a16e30a64ce80fa4b47de1797c9
Lorenzo Mazzarotto <usomenac@icloud.com>
2024-08-01 21:41:17 -0300

    Set up navigation between list and detail

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,48 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Lorenzo Mazzarotto on 01/08/24.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
