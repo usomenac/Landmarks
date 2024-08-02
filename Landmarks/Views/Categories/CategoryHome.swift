0620bba243a8914d9d4ab178d8fe22adf12551c9
Lorenzo Mazzarotto <usomenac@icloud.com>
2024-08-02 00:23:19 -0300

    Add a category view

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Lorenzo Mazzarotto on 02/08/24.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationSplitView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+}
