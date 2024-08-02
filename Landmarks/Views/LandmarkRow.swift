b76a6925d07e325971b8478d2000a9dd1fdea2b5
Lorenzo Mazzarotto <usomenac@icloud.com>
2024-08-01 22:25:36 -0300

    Adopt the model object in your views

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -15,12 +15,14 @@ struct LandmarkRow: View {
             landmark.image
                 .resizable()
                 .frame(width: 50, height: 50)
+                .cornerRadius(5)
             Text(landmark.name)
             
             Spacer()
             
             if landmark.isFavorite {
                 Image(systemName: "star.fill")
+                    .imageScale(.medium)
                     .foregroundStyle(.yellow)
             }
         }
@@ -28,7 +30,8 @@ struct LandmarkRow: View {
 }
 
 #Preview {
-    Group {
+    let landmarks = ModelData().landmarks
+    return Group {
         LandmarkRow(landmark: landmarks[0])
         LandmarkRow(landmark: landmarks[1])
     }
