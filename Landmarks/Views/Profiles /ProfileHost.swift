53aa7495239fb6a94dafb2eefe1c3034bd9b4491
Lorenzo Mazzarotto <usomenac@icloud.com>
2024-08-02 00:42:17 -0300

    Display a user profile

---
+++ Landmarks/Views/Profiles /ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Lorenzo Mazzarotto on 02/08/24.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
