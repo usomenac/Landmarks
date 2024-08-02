c26202ec9e96b9ccd4d9d4141ee71a8e9fb4681c
Lorenzo Mazzarotto <usomenac@icloud.com>
2024-08-02 00:06:48 -0300

    Add hiking data to the app

---
+++ Landmarks/Views/Hikes/HikeView.swift
@@ -0,0 +1,52 @@
+//
+//  HikeView.swift
+//  Landmarks
+//
+//  Created by Lorenzo Mazzarotto on 02/08/24.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var hike: Hike
+    @State private var showDetail = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                HikeGraph(hike: hike, path: \.elevation)
+                    .frame(width: 50, height: 30)
+                
+                VStack(alignment: .leading) {
+                    Text(hike.name)
+                        .font(.headline)
+                    Text(hike.distanceText)
+                }
+                
+                Spacer()
+                
+                Button {
+                    showDetail.toggle()
+                } label: {
+                    Label("Graph", systemImage: "chevron.right.circle")
+                        .labelStyle(.iconOnly)
+                        .imageScale(.large)
+                        .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        .padding()
+                }
+            }
+            
+            if showDetail {
+                HikeDetail(hike: hike)
+            }
+        }
+    }
+}
+
+#Preview {
+    VStack {
+        HikeView(hike: ModelData().hikes[0])
+            .padding()
+        Spacer()
+    }
+}
